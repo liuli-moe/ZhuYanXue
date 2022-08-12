54bb6409319b4be4d49feb7bc1635f27daf80b01
rxliuli <rxliuli@gmail.com>
2022-08-12 13:46:45 +0800

    feat: 更新 mdbook

--- build.ts
+++ build.ts
@@ -1,27 +1,30 @@
 import { AsyncArray } from '@liuli-util/async'
 import { mkdirp, readdir, remove, stat } from 'fs-extra'
 import * as path from 'path'
-import { exec, ExecOptions } from 'child_process'
-
-function execPromise(
-  command: string,
-  options?: ExecOptions,
-): Promise<string | Buffer> {
-  return new Promise((resolve, reject) => {
-    exec(command, options, (error, stdout) => {
-      if (error) {
-        reject(error)
-        return
-      }
-      resolve(stdout)
-    })
-  })
-}
+import { BookConfig, MarkdownBookBuilder } from '@liuli-util/mdbook'
+import matter from 'gray-matter'
+import JSZip from 'jszip'
+import { readFile, writeFile } from 'fs/promises'
+import FastGlob from 'fast-glob'
 
 async function clean() {
   const distPath = path.resolve(__dirname, '../dist')
   await remove(distPath)
   await mkdirp(distPath)
+}
+
+async function bundle() {
+  console.log('打包 zip 文件')
+  const distPath = path.resolve(__dirname, '../dist')
+  const list = await FastGlob('*.epub', { cwd: distPath })
+  const zip = new JSZip()
+  await AsyncArray.forEach(list, async (name) => {
+    zip.file(name, await readFile(path.resolve(distPath, name)))
+  })
+  await writeFile(
+    path.resolve(distPath, 'books.zip'),
+    await zip.generateAsync({ type: 'nodebuffer' }),
+  )
 }
 
 async function build() {
@@ -35,17 +38,23 @@ async function build() {
       )
     },
   )
+  const builder = new MarkdownBookBuilder()
+  const distPath = path.resolve(__dirname, '../dist')
   for (const name of list) {
     console.log(`构建 [${name}]`)
-    await execPromise('npx mdbook build -o ../../dist', {
-      cwd: path.resolve(booksPath, name),
-    })
+    const entryPoint = path.resolve(booksPath, name, 'README.md')
+    const title = (matter(await readFile(entryPoint)).data as BookConfig).title
+    await writeFile(
+      path.resolve(distPath, title + '.epub'),
+      await builder.generate(entryPoint),
+    )
   }
 }
 
 async function main() {
   await clean()
   await build()
+  await bundle()
 }
 
 main()
